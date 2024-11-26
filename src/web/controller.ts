a78964426ce31733a628259f3a132a9e0b2859e3
David Alecrim <dsalecrim@outlook.com>
2024-11-25 21:49:55 -0300

    fix: validation bug

--- src/web/controller.ts
+++ src/web/controller.ts
@@ -2,7 +2,7 @@ import { Person } from "../app/domain.ts";
 import { PersonAlreadyExists } from "../app/errors.ts";
 import logger from "../infra/logger.ts";
 import PersonService from "../app/service.ts";
-import { Context, RouterContext } from "https://deno.land/x/oak@v17.1.3/mod.ts";
+import { RouterContext } from "https://deno.land/x/oak@v17.1.3/mod.ts";
 
 class PersonController {
   personService: PersonService;
@@ -15,13 +15,15 @@ class PersonController {
       const id = ctx.params.id;
 
       if (!id) {
-        return ctx.response.status = 400;
+        ctx.response.status = 400;
+        return;
       }
 
       const result = await this.personService.GetPersonById(id);
 
-      if (!result) {
-        return ctx.response.status = 404;
+      if (!result || result === undefined) {
+        ctx.response.status = 404;
+        return;
       }
 
       ctx.response.status = 200;
@@ -47,21 +49,22 @@ class PersonController {
 
       if (
         data.apelido === null || data.apelido === undefined ||
-        data.nome === null || data.nome === undefined
-      ) {
-        return ctx.response.status = 422;
-      }
-
-      if (
-        data.apelido.length > 32 || data.nome.length > 32 ||
+        data.nome === null || data.nome === undefined ||
+        data.apelido.length > 32 || data.nome.length > 100 ||
         !dateRegex.test(data.nascimento)
       ) {
-        return ctx.response.status = 422;
+        logger.debug(`CreatePerson - invalid request: ${JSON.stringify(data)}`);
+        ctx.response.status = 422;
+        return;
       }
 
       for (const stack of data.stack ?? []) {
         if (stack.length > 32) {
-          return ctx.response.status = 422;
+          logger.debug(
+            `CreatePerson - invalid request: ${JSON.stringify(data)}`,
+          );
+          ctx.response.status = 422;
+          return;
         }
       }
 
@@ -81,36 +84,43 @@ class PersonController {
       });
     } catch (err) {
       if (err instanceof PersonAlreadyExists) {
-        return ctx.response.status = 422;
+        logger.debug(`CreatePerson - person already exists: ${err}`);
+        ctx.response.status = 422;
+        return;
       }
       if (err instanceof Error) {
         logger.error(`error in createPerson: ${err.stack}`);
         ctx.response.status = 500;
+        return;
       }
+      logger.error(err);
     }
   }
 
-  async CountPeople(ctx: Context) {
+  async CountPeople(ctx: RouterContext<"/contagem-pessoas">) {
     try {
       ctx.response.status = 200;
       ctx.response.body = await this.personService.GetPersonsCount();
     } catch (err) {
       if (err instanceof Error) {
         logger.error(`error in countPeople: ${err.stack}`);
+        ctx.response.status = 500;
+        return;
       }
-      ctx.response.status = 500;
+      logger.error(err);
     }
   }
 
-  async SearchPeople(ctx: Context) {
+  async SearchPeople(ctx: RouterContext<"/pessoas">) {
     try {
       const term = ctx.request.url.searchParams.get("t");
-      if (!term) {
-        return ctx.response.status = 400;
+      if (!term || term.trim() === "") {
+        ctx.response.status = 400;
+        return;
       }
 
       const people = await this.personService.SearchPeople(term);
-      const response = people.map((person: any) => ({
+      const response = people.map((person: Person) => ({
         id: person.id,
         apelido: person.nickname,
         nome: person.name,
@@ -124,7 +134,9 @@ class PersonController {
       if (err instanceof Error) {
         logger.error(`error in SearchPeople: ${err.stack}`);
         ctx.response.status = 500;
+        return;
       }
+      logger.error(err);
     }
   }
 }
